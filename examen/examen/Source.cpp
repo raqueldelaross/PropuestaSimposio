ee4e81da52138e0a58f3017bd5f5787fb05434cf
Raquel De la Rosa <78809531+raqueldelaross@users.noreply.github.com>
2021-03-23 10:04:00 -0600

    Add files via upload

---
+++ examen/examen/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cout << "Ingrese un número de estudiantes\n";
+	cin >> n;
+	if ((n % 4 = 0)) {
+		cout << "Los estudiantes deberán asistir a clases de modalidad presencial\n";
+	}
+	else {
+		cout << "Los estudiantes deberán asistir a clases de modalidad virtual\n"
+	}
+		 
+}
